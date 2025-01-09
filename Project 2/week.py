c259e7aeb1081cb8bda00bd23dfd3ba0eef3916e
Isfanchik16 <111650885+Isfanchik16@users.noreply.github.com>
2025-01-09 20:05:08 +0500

    Create week.py

---
+++ Project 2/week.py
@@ -0,0 +1,17 @@
+week = int(input("Welcome to the Days of the Week calculator. Enter a number (1-7) : "))
+if week == 1:
+    print("Monday")
+elif week == 2:
+    print("Tuesday")
+elif week == 3:
+    print("Wednesday")
+elif week == 4:
+    print("Thursday")
+elif week == 5:
+    print("Friday")
+elif week == 6:
+    print("Saturday")
+elif week == 7:
+    print("Sunday")
+else:
+    print("Please Enter from 1 to 7 numbers. Thanks!")
