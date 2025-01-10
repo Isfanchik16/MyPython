9f9c48a3eb5e852bfeb813f2e40404714e2e8523
Isfanchik16 <111650885+Isfanchik16@users.noreply.github.com>
2025-01-10 21:22:10 +0500

    Mini

---
+++ Project 3/list.py
@@ -0,0 +1,10 @@
+def ele_list(lenght):
+    res=[]
+    i=0
+    while i<lenght:
+        string = "Enter elements "+"#"+str(i+1)+": "
+        res.append(input(string))
+        i+=1
+    return res
+lenght=int(input("Enter lenght of the list : "))
+print(ele_list(lenght))
