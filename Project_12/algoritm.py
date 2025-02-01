8a595d7adc36fd3caf46b6005e05cc8c6b1b2d68
Isfanchik16 <azimboevisfandier@gmail.com>
2025-02-01 22:53:06 +0500

    Mini

--- Project_12/algoritm.py
+++ Project_12/algoritm.py
@@ -1,17 +1,44 @@
+# Greatest common divisor
 def gcd(m,n):
     if m<n:m,n=n,m
     rem = m % n
     if rem==0:
         return n
     return gcd(n,rem)
+print(gcd(26,12))
 
+# Factorial
 def factorical(number):
     if (number<=1):return 1
     return number*factorical(number-1)
+print(factorical(5))
 
+# Fibonacci Series
 def fibonacci_series(m):
     if m ==0 or m==1:return 1
     return fibonacci_series(m-1)+fibonacci_series(m-2)
-print(gcd(26,12))
-print(factorical(5))
+
 print(fibonacci_series(5))
+
+# Bubble Sort Improvement
+def bubble_1(lst): 
+    len_lst = len(lst)-1
+    sorted = False
+    while not sorted:
+        sorted = True
+        for x in range(len_lst):
+            if lst[x] > lst[x+1]:
+                lst[x],lst[x+1] = lst[x+1],lst[x]
+                sorted = False
+    return lst
+print(bubble_1([4,8,5,3,2,1]))
+
+# Bubble Sort Implementation Implementation 
+def bubble_2(lst):
+    for i in range(len(lst)-1):
+        for j in range(len(lst)-1-i):
+            if lst[j] > lst[j+1]:
+                lst[j],lst[j+1] = lst[j+1],lst[j]
+    return lst
+print(bubble_2([1,2,3,4,5]))
+
--- Project_13/gpa.py
+++ Project_13/gpa.py
@@ -1,1 +1,20 @@
-print("Noting!")
+def calculate_gpa(grade):
+    gpa = 0.0
+    for x in grade:
+        if x == 'A+':gpa +=4.5
+        elif x.upper() == 'A0': gpa +=4.0
+        elif x.upper() == 'B+': gpa +=3.5
+        elif x.upper() == 'B0': gpa +=3.0
+        elif x.upper() == 'C+': gpa +=2.5
+        elif x.upper() == 'C0': gpa +=2.0
+        elif x.upper() == 'D+': gpa +=1.5
+        elif x.upper() == 'D0': gpa +=1.0
+        elif x.upper() == 'F':  gpa +=0.0
+    return round(gpa/len(grade),2)
+
+your_grade=[]
+num_sub = int(input("How many subjects did you have in Uni? "))
+for x in range(num_sub):
+    your_grade +=[input(f"Your {x+1} grade: ")]
+print(f"Overall Your GPA is {calculate_gpa(your_grade)}")
+        
