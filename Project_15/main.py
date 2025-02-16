bc90624343a33525dc9d08e7e1dd76b91837acaf
Isfandiyor-Az <azimboevisfandier@gmail.com>
2025-02-16 12:10:01 +0500

    Mini

--- Project_15/main.py
+++ Project_15/main.py
@@ -62,4 +62,45 @@ print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
 
 
 
+
+def removeDuplicates(nums):
+        s = set(nums)
+        nums.clear()
+        nums = [x for x in s]
+        return len(nums)
+
+print(removeDuplicates([1,1,2]))
+
+def removeDuplicates(nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)==0:return 0
+        i,j = 0,1
+        while j<len(nums):
+            if nums[i] != nums[j]:
+                i +=1
+                nums[i] = nums[j]
+                print(nums[i])
+            j+=1
+        return i+1
+print(removeDuplicates([0,0,1,1,1,2,2,3,3,4,5]))
+
+
+def removeDuplicates1(nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)==0:return 0
+        i=0
+        for j in range(1,len(nums)):
+                if nums[j] != nums[i]:
+                        i +=1
+                        nums[i] = nums[j]
+        return i
+print(removeDuplicates1([0,0,1,1,1,2,2,3,3,4,5]))
+
+
     
